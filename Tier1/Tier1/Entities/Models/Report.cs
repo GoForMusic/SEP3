39035b0d40e929c4d5b80fb00eff0981e7b64c65
VassilevEmil <84077074+VassilevEmil@users.noreply.github.com>
2022-05-17 18:58:58 +0200

    admin view done as well as adding the methods for http client but without implementation since it is not my task

--- Tier1/Tier1/Entities/Models/Report.cs
+++ Tier1/Tier1/Entities/Models/Report.cs
@@ -2,4 +2,7 @@
 
 public class Report {
     public int  PostId { get; set; }
+    public int UserId { get; set; }
+    public string reason { get; set; }
+    public int reportId { get; set; }
 }
---
+++ Tier1/Tier1/HttpServices/ReportHttpsCLient.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace HttpServices;
+
+public class ReportHttpsCLient : IReportService
+{
+    public Task AddReport(Report report)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveReport(Report report)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Report>> GetAllReports()
+    {
+        throw new NotImplementedException();
+    }
+}
