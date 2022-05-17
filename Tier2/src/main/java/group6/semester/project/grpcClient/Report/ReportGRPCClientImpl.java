e3defca1e38024f178eab9f4891f2cf4412ad083
VassilevEmil <84077074+VassilevEmil@users.noreply.github.com>
2022-05-17 20:26:42 +0200

    packages and stuff done

---
+++ Tier2/src/main/java/group6/semester/project/grpcClient/Report/ReportGRPCClientImpl.java
@@ -0,0 +1,22 @@
+package group6.semester.project.grpcClient.Report;
+
+import group6.semester.project.model.Report;
+
+import java.util.List;
+
+public class ReportGRPCClientImpl implements ReportClient {
+    @Override
+    public void AddReport(Report report) {
+
+    }
+
+    @Override
+    public void RemoveReport(Report report) {
+
+    }
+
+    @Override
+    public List<Report> getReports() {
+        return null;
+    }
+}
---
+++ Tier3/EFC/DAOImpl/ReportDAOImpl.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Contracts;
+using Entities.Models;
+
+namespace EFC.DAOImpl;
+
+public class ReportDAOImpl : IReportService
+{
+    public Task<Report> AddReport(Report report)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Bookmark> RemoveReport(Report report)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Report>> getReports()
+    {
+        throw new NotImplementedException();
+    }
+}
