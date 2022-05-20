b9b046a115c4ee54d133f6353f256a3189043d67
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-05-20 23:04:35 +0200

    Unversioned

---
+++ Tier3/EFC/Migrations/20220514212557_fix4.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace EFC.Migrations
+{
+    public partial class fix4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Bookmarks",
+                table: "Bookmarks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookmarks_PostId",
+                table: "Bookmarks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Bookmarks",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Bookmarks",
+                table: "Bookmarks",
+                columns: new[] { "PostId", "Username" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Bookmarks",
+                table: "Bookmarks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Bookmarks",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Bookmarks",
+                table: "Bookmarks",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookmarks_PostId",
+                table: "Bookmarks",
+                column: "PostId");
+        }
+    }
+}
