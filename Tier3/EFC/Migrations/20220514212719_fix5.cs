238a16063c551dae6e594ac65f76582893d3dc5e
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-05-20 23:04:35 +0200

    Unversioned

---
+++ Tier3/EFC/Migrations/20220514212719_fix5.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFC.Migrations
+{
+    public partial class fix5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Bookmarks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Bookmarks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
